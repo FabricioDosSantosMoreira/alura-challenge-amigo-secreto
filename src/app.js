5485e3b13e0d7e94fb693d9e44deacfc52de727c
Fabrício dos Santos Moreira <dev.fabriciodossantosmoreira@gmail.com>
2025-01-24 10:31:40 -0300

    [UPDATE] - Projeto

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+var nomes = [];
+
+function renderizarLista() {
+    var lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < nomes.length; i++) {
+        var item = document.createElement('li');
+        item.textContent = nomes[i];
+        lista.appendChild(item);
+
+        console.log(lista);
+    }
+}
+
+function adicionarAmigo() {
+    var input = document.getElementById('input-amigo');
+    var nome = input.value;
+
+    nomes.push(nome);
+    input.value = '';
+
+    renderizarLista();
+}
+
+function sortearAmigo() {
+    var sorteado = nomes[Math.floor(Math.random() * nomes.length)];
+
+    var resultado = document.getElementById('resultado');
+    resultado.textContent = sorteado;
+}
